019945f12195e687ab5695ff236cb1b6af70450c
Shalise Ayromloo <shaliseayromloo@shalises-macbook-air.local>
2019-03-22 21:17:15 -0500

    This app is based on lab 4.4 in App Development With Swift Textbook. It implements a scroll view on an image that will enable users to zoom in on the image.

--- ISpy/ViewController.swift
+++ ISpy/ViewController.swift
@@ -8,11 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIScrollViewDelegate {
+    
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        scrollView.delegate = self
+        updateZoomFor(size: view.bounds.size)
+    }
+    
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+    
+    func updateZoomFor(size: CGSize) {
+        let widthScale = size.width / imageView.bounds.width
+        let heightScale = size.height / imageView.bounds.height
+        let scale = min(widthScale, heightScale)
+        scrollView.minimumZoomScale = scale
+//        scrollView.minimumZoomScale = 1.0
+//        scrollView.maximumZoomScale = 5.0
+//        
+//        scrollView.contentSize = .init(width: 2000, height = 2000)
+        
     }
 
 
